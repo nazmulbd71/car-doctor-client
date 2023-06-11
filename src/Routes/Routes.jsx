00882ba0bbdf830be08b64841d72c15435da66e6
Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-06-11 15:46:47 +0600

    created main component and set routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Main />,
     },
 ]);
 
